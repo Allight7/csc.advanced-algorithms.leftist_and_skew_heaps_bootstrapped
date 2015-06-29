958ed64d68e6092a9e024e2781bed75d373bc46b
Allight7 <allight.me@ya.ru>
2015-06-29 21:54:07 +0300

    common test improvements

--- test_param.h
+++ test_param.h
@@ -1,5 +1,5 @@
 #ifndef _TEST_PARAM_
 #define _TEST_PARAM_
-	const int HEAP_SIZE = 5e4;
+	const int HEAP_SIZE = 5e3;
 	const int HEAPS_NUM = 1e1;
 #endif
--- tests_compare/test_tl_compare.cpp
+++ tests_compare/test_tl_compare.cpp
@@ -28,233 +28,219 @@ void gen( int n, std::vector <int> &a ) {
 #define timeStamp(...) fprintf(stderr, __VA_ARGS__, (clock() - start) / CLOCKS_PER_SEC), start = clock()
 
 int main() {
-  // std::vector <int> x;
-  int n = HEAP_SIZE;
-  int k = HEAPS_NUM;
-  // gen(n, x);
-  // timeStamp("input is generated: %.2f\n");
-
-  // h.build(n, x.begin());
-  // // forn(i, n)
-  // //   h.add(x[i]);
-  // timeStamp("leftist_heap \tbuilded: \t%.2f\n");
-  // forn(i, n)
-  //   h.extractMin();
-  // timeStamp("leftist_heap \textracted: \t%.2f\n");
-
-
-  // priority_queue<int, std::vector<int>, greater<int>> q;
-  // forn(i, n)
-  //   q.push(x[i]);
-  // timeStamp("priority_queue \tbuilded: \t%.2f\n");
-  // forn(i, n)
-  //   q.pop();
-  // timeStamp("priority_queue \textracted: \t%.2f\n");
-
-  // multiset<int> s;
-  // forn(i, n)
-  //   s.insert(x[i]);
-  // timeStamp("set \t\tbuilded: \t%.2f\n");
-  // forn(i, n)
-  //   s.erase(s.begin());
-  // timeStamp("set \t\textracted: \t%.2f\n");
+  // int n = HEAP_SIZE;
+  // int k = HEAPS_NUM;
 
   double start = 0;
 
-  std::vector <std::vector <int>> x(k);
-  std::vector <LHeap<unsigned>>         hl(k);
-  std::vector <LHeapBoot<unsigned>>     hla(k);
-  std::vector <LeftistHeapMergeBootInt> hlam(k);
-  std::vector <SHeap<unsigned>>         hs(k);
-  std::vector <SHeapBoot<unsigned>>     hsa(k);
-  std::vector <SkewHeapMergeBootInt>    hsam(k);
-  std::vector< std::priority_queue<int, std::vector<int>, std::greater<int>>> q(k);
-
+  forn(a,1){
+    fprintf(stderr, "a = %d\n",a);
   
-  forn(i,k)
-    gen(n,x[i]);
-  timeStamp("input is generated: %.2f\n");
+    forn(b,2){
+      int n = 1; //heap size;
+      int k = 1; //heaps num;
+      int * p = b ? &n : &k;
+    
+      for(*p = (int)1e4; *p < (int)1e6; *p *= 10){
+        fprintf(stderr, "\n============================================================\n");
+        fprintf(stderr, "k = %d, n = %d\n",k,n);
+        std::vector <std::vector <int>> x(k);
+        std::vector <LHeap<unsigned>>         hl(k);
+        std::vector <LHeapBoot<unsigned>>     hla(k);
+        std::vector <LeftistHeapMergeBootInt> hlam(k);
+        std::vector <SHeap<unsigned>>         hs(k);
+        std::vector <SHeapBoot<unsigned>>     hsa(k);
+        std::vector <SkewHeapMergeBootInt>    hsam(k);
+        std::vector< std::priority_queue<int, std::vector<int>, std::greater<int>>> q(k);
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nleftist\n\n");
+        forn(i,k)
+          gen(n,x[i]);
+        timeStamp("gen: %.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hl[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hl[0].mergeWithHeap(hl[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+        // ----------------------------------------- //
+        fprintf(stderr, "\n\nleftist\n\n");
 
-  hl[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hl[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hl[0].empty())
-    hl[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hl[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hl[0].mergeWithHeap(hl[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nleftist add\n\n");
+        hl[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hla[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
+        while (!hl[0].empty())
+          hl[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hl[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hla[0].mergeWithHeap(hla[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nleftist add\n\n");
 
-  hla[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hla[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hla[0].empty())
-    hla[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hla[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hla[0].mergeWithHeap(hla[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nleftist add merge\n\n");
+        hla[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hlam[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
+        while (!hla[0].empty())
+          hla[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hla[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hlam[0].mergeWithHeap(hlam[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nleftist add merge\n\n");
 
-  hlam[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hlam[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hlam[0].empty())
-    hlam[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hlam[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hlam[0].mergeWithHeap(hlam[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nskew\n\n");
+        hlam[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hs[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
+        while (!hlam[0].empty())
+          hlam[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hlam[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hs[0].mergeWithHeap(hs[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nskew\n\n");
 
-  hs[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hs[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hs[0].empty())
-    hs[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hs[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hs[0].mergeWithHeap(hs[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nskew add\n\n");
+        hs[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hsa[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
+        while (!hs[0].empty())
+          hs[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hs[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hsa[0].mergeWithHeap(hsa[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nskew add\n\n");
 
-  hsa[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hsa[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hsa[0].empty())
-    hsa[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hsa[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hsa[0].mergeWithHeap(hsa[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nskew add merge\n\n");
+        hsa[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i,k)
-    forn(j, n)
-      hsam[i].add(x[i][j]);
-  timeStamp("k heaps builded: \t%.2f\n");
+        while (!hsa[0].empty())
+          hsa[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hsa[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    hsam[0].mergeWithHeap(hsam[i]);
-  timeStamp("k heaps merged: \t%.2f\n");
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nskew add merge\n\n");
 
-  hsam[0].pop();
-  timeStamp("heap firts acc: \t%.2f\n");
+        forn(i,k)
+          forn(j, n)
+            hsam[i].add(x[i][j]);
+        timeStamp("k heaps builded: \t%.2f\n");
 
-  while (!hsam[0].empty())
-    hsam[0].pop();     
-  timeStamp("heap extracted: \t%.2f\n");
-  
-  forn(i,k)
-    hsam[i].clear();
-  timeStamp("cleared: \t%.2f\n");
+        for(int i = 1; i < k; ++i)
+          hsam[0].mergeWithHeap(hsam[i]);
+        timeStamp("k heaps merged: \t%.2f\n");
 
-// ----------------------------------------- //
-  fprintf(stderr, "\n\npriority queue\n\n");
+        hsam[0].pop();
+        timeStamp("heap firts acc: \t%.2f\n");
 
-  forn(i, k)
-    forn(j, n)
-      q[i].push(x[i][j]);
-  timeStamp("k queues builded: \t%.2f\n");
+        while (!hsam[0].empty())
+          hsam[0].pop();     
+        timeStamp("heap extracted: \t%.2f\n");
+        
+        forn(i,k)
+          hsam[i].clear();
+        timeStamp("cleared: \t%.2f\n");
 
-  for(int i = 1; i < k; ++i)
-    forn(j, n){
-      q[0].push(q[i].top());
-      q[i].pop();
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\npriority queue\n\n");
+
+        forn(i, k)
+          forn(j, n)
+            q[i].push(x[i][j]);
+        timeStamp("k queues builded: \t%.2f\n");
+
+        for(int i = 1; i < k; ++i)
+          forn(j, n){
+            q[0].push(q[i].top());
+            q[i].pop();
+          }
+        timeStamp("k queues merged: \t%.2f\n");
+
+        q[0].pop();
+        timeStamp("queue firts acc: \t%.2f\n");
+        
+        while (!q[0].empty())
+          q[0].pop();     
+        timeStamp("queue extracted: \t%.2f\n");
+
+      // ----------------------------------------- //
+        fprintf(stderr, "\n\nset\n\n");
+
+        std::vector<std::multiset<int>> s(k);
+        forn(i, k)
+          forn(j, n)
+            s[i].insert(x[i][j]);
+        timeStamp("k sets builded: \t%.2f\n");
+
+        for(int i = 1; i < k; ++i)
+          forn(j, n){
+            s[0].insert(*(s[i].begin()));
+            s[i].erase(s[i].begin());
+          }
+        timeStamp("k sets merged: \t\t%.2f\n");
+
+        s[0].erase(s[0].begin());
+        timeStamp("set firts acc: \t\t%.2f\n");
+        
+        while (!s[0].empty())
+        s[0].erase(s[0].begin());
+        timeStamp("set extracted: \t\t%.2f\n");
+      }
     }
-  timeStamp("k queues merged: \t%.2f\n");
-
-  q[0].pop();
-  timeStamp("queue firts acc: \t%.2f\n");
-  
-  while (!q[0].empty())
-    q[0].pop();     
-  timeStamp("queue extracted: \t%.2f\n");
-
-// ----------------------------------------- //
-  fprintf(stderr, "\n\nset\n\n");
-
-  std::vector<std::multiset<int>> s(k);
-  forn(i, k)
-    forn(j, n)
-      s[i].insert(x[i][j]);
-  timeStamp("k sets builded: \t\t%.2f\n");
-
-  for(int i = 1; i < k; ++i)
-    forn(j, n){
-      s[0].insert(*(s[i].begin()));
-      s[i].erase(s[i].begin());
-    }
-  timeStamp("k sets merged: \t\t%.2f\n");
-
-  q[0].pop();
-  timeStamp("queue firts acc: \t%.2f\n");
-  
-  while (!s[0].empty())
-  s[0].erase(s[0].begin());
-  timeStamp("set \t\tfully extracted: \t%.2f\n");
+  }
 }
